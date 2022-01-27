cc16ba6830c07a42bb661ba0032e97a4fa0bc2c8
kangkc09 <kangkc09@pusan.ac.kr>
2022-01-27 17:48:59 +0900

    feat) print coordiates

--- CamelPathFinding.py
+++ CamelPathFinding.py
@@ -15,6 +15,7 @@ height = 700
 horizontalStepCount = 100
 verticalStepCount = imageheight//(imagewidth//horizontalStepCount)
 length = 14
+
 Color = {0: "none", 1: "white", 2: "green", 3: "red"}  # 열린목록 닫힌목록 길 등은 나중에 추가
 
 
@@ -62,7 +63,7 @@ class AStarPathFinding:
                                 "setbg : set background image and coordinates\n"+
                                 "help : show details of commands\n"+
                                 "clear : clean up the terminal log\n")
-        Button(self.window, text="none", font=36, fg="black", background="white", height = 2, width = 6,
+        Button(self.window, text="좌표", font=36, fg="black", background="white", height = 2, width = 6,
                command=self.BTnone).place(x=400, y=630)
         Button(self.window, text="wall", font=36, fg="black", background="white", height = 2, width = 6,
                command=self.BTwall).place(x=520, y=630)
@@ -106,12 +107,13 @@ class AStarPathFinding:
 
     # reset board
     def reset(self):
-        self.imgcanvas.delete("all")
-        self.imgcanvas.create_image(0,0, anchor=NW,image=self.photo)
-        self.boardZero()
-        self.modenumber = 0
-        self.startcount = 0
-        self.goalcount = 0
+        if self.boradOn==True :
+            self.imgcanvas.delete("all")
+            self.imgcanvas.create_image(0,0, anchor=NW,image=self.photo)
+            self.boardZero()
+            self.modenumber = 0
+            self.startcount = 0
+            self.goalcount = 0
 
     def mainloop(self):
         self.window.mainloop()
@@ -120,154 +122,158 @@ class AStarPathFinding:
     # Button Command function
     # ---------------------------------------------------------
     def BTnone(self):
-        self.modenumber = 0
+        if self.boradOn==True :
+            self.modenumber = 0
 
     def BTwall(self):
-        self.modenumber = 1
-        # self.initialize_board()
+        if self.boradOn==True :
+            self.modenumber = 1
 
     def BTstart(self):
-        self.modenumber = 2
+        if self.boradOn==True :
+            self.modenumber = 2
 
     def BTgoal(self):
-        self.modenumber = 3
+        if self.boradOn==True :
+            self.modenumber = 3
 
     def BTastar(self):
-        startflag = np.any(self.board == 2)
-        goalflag = np.any(self.board == 3)
+        if self.boradOn==True :
+            startflag = np.any(self.board == 2)
+            goalflag = np.any(self.board == 3)
 
-        if not (startflag and goalflag):
-            messagebox.showinfo("Notice", "Please select start and goal point")
+            if not (startflag and goalflag):
+                messagebox.showinfo("Notice", "Please select start and goal point")
 
-        class Node:
-            def __init__(self, parent=None, position=None):
-                self.parent = parent
-                self.position = position
+            class Node:
+                def __init__(self, parent=None, position=None):
+                    self.parent = parent
+                    self.position = position
 
-                self.g = 0
-                self.h = 0
-                self.f = 0
+                    self.g = 0
+                    self.h = 0
+                    self.f = 0
 
-            def __eq__(self, other):
-                return self.position == other.position
+                def __eq__(self, other):
+                    return self.position == other.position
 
-        def aStar(maze, start, end):
-            startNode = Node(None, start)
-            endNode = Node(None, end)
+            def aStar(maze, start, end):
+                startNode = Node(None, start)
+                endNode = Node(None, end)
 
-            # openList, closedList 초기화
-            openList = []
-            closedList = []
+                # openList, closedList 초기화
+                openList = []
+                closedList = []
 
-            # openList에 시작 노드 추가
-            openList.append(startNode)
+                # openList에 시작 노드 추가
+                openList.append(startNode)
 
-            # endNode를 찾을 때까지 실행
-            while openList:
-                # 현재 노드 지정
-                currentNode = openList[0]
-                currentIdx = 0
+                # endNode를 찾을 때까지 실행
+                while openList:
+                    # 현재 노드 지정
+                    currentNode = openList[0]
+                    currentIdx = 0
 
-                # 이미 같은 노드가 openList에 있고, f 값이 더 크면
-                # currentNode를 openList안에 있는 값으로 교체
-                for index, item in enumerate(openList):
-                    if item.f < currentNode.f:
-                        currentNode = item
-                        currentIdx = index
-                # openList에서 제거하고 closedList에 추가
-                openList.pop(currentIdx)
-                closedList.append(currentNode)
-                def drawnode(currentNode):
-                    self.imgcanvas.create_rectangle(
-                        (currentNode.position[1])*length, (currentNode.position[0])*length, (currentNode.position[1]+1)*length, (currentNode.position[0]+1)*length, fill="lawn green")
-                    self.window.update()  
-                                       
-                self.window.update()
-                self.window.after(10,drawnode(currentNode))
-                # 현재 노드가 목적지면 current.position 추가하고
-                # current의 부모로 이동
-                def findpath(cell):
-                        if cells.index(cell) == 0:
-                            self.imgcanvas.create_rectangle(
-                                (cell[1])*length, (cell[0])*length, (cell[1]+1)*length, (cell[0]+1)*length, fill="green")
-                        elif cells.index(cell) == (len(cells) - 1):
-                            self.imgcanvas.create_rectangle(
-                                (cell[1])*length, (cell[0])*length, (cell[1]+1)*length, (cell[0]+1)*length, fill="red")
-                        else:
-                            self.imgcanvas.create_rectangle(
-                                (cell[1])*length, (cell[0])*length, (cell[1]+1)*length, (cell[0]+1)*length, fill="yellow")
-                        self.window.update()
-                if currentNode.position == endNode.position:
-                    path = []
-                    current = currentNode
-                    while current is not None:
-                        path.append(current.position)
-                        current = current.parent
-                    cells = path[::-1]
-                    for cell in cells:
-                        self.window.update()
-                        self.window.after(10,findpath(cell))
-                    return
-
-                children = []
-                # 인접한 xy좌표 전부
-                for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-
-                    # 노드 위치 업데이트
-                    nodePosition = (
-                        currentNode.position[0] + newPosition[0],  # X
-                        currentNode.position[1] + newPosition[1])  # Y
-
-                    # 미로 maze index 범위 안에 있어야함
-                    within_range_criteria = [
-                        nodePosition[0] > (len(maze) - 1),
-                        nodePosition[0] < 0,
-                        nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
-                        nodePosition[1] < 0,
-                    ]
-
-                    if any(within_range_criteria):  # 하나라도 true면 범위 밖임
-                        continue
-
-                    # 장애물이 있으면 다른 위치 불러오기
-                    if maze[nodePosition[0]][nodePosition[1]] == 1:
-                        continue
-
-                    new_node = Node(currentNode, nodePosition)
-                    children.append(new_node)
-
-                # 자식들 모두 loop
-                for child in children:
-
-                    # 자식이 closedList에 있으면 continue
-                    if child in closedList:
-                        continue
-
-                    # f, g, h값 업데이트
-                    child.g = currentNode.g + 1
-                    child.h = ((child.position[0] - endNode.position[0]) **
-                               2) + ((child.position[1] - endNode.position[1]) ** 2)
-                    child.f = child.g + child.h
-
-                    # 자식이 openList에 있으고, g값이 더 크면 continue
-                    if len([openNode for openNode in openList
-                            if child == openNode and child.g > openNode.g]) > 0:
-                        continue
-                    
-                    def drawlist(child):
+                    # 이미 같은 노드가 openList에 있고, f 값이 더 크면
+                    # currentNode를 openList안에 있는 값으로 교체
+                    for index, item in enumerate(openList):
+                        if item.f < currentNode.f:
+                            currentNode = item
+                            currentIdx = index
+                    # openList에서 제거하고 closedList에 추가
+                    openList.pop(currentIdx)
+                    closedList.append(currentNode)
+                    def drawnode(currentNode):
                         self.imgcanvas.create_rectangle(
-                        (child.position[1])*length, (child.position[0])*length, (child.position[1]+1)*length, (child.position[0]+1)*length, fill="dodger blue")
-                        self.window.update()
+                            (currentNode.position[1])*length, (currentNode.position[0])*length, (currentNode.position[1]+1)*length, (currentNode.position[0]+1)*length, fill="lawn green")
+                        self.window.update()  
+                                        
                     self.window.update()
-                    self.window.after(10,drawlist(child))
-                    openList.append(child)
-                    # time.sleep(0.5)
+                    self.window.after(10,drawnode(currentNode))
+                    # 현재 노드가 목적지면 current.position 추가하고
+                    # current의 부모로 이동
+                    def findpath(cell):
+                            if cells.index(cell) == 0:
+                                self.imgcanvas.create_rectangle(
+                                    (cell[1])*length, (cell[0])*length, (cell[1]+1)*length, (cell[0]+1)*length, fill="green")
+                            elif cells.index(cell) == (len(cells) - 1):
+                                self.imgcanvas.create_rectangle(
+                                    (cell[1])*length, (cell[0])*length, (cell[1]+1)*length, (cell[0]+1)*length, fill="red")
+                            else:
+                                self.imgcanvas.create_rectangle(
+                                    (cell[1])*length, (cell[0])*length, (cell[1]+1)*length, (cell[0]+1)*length, fill="yellow")
+                            self.window.update()
+                    if currentNode.position == endNode.position:
+                        path = []
+                        current = currentNode
+                        while current is not None:
+                            path.append(current.position)
+                            current = current.parent
+                        cells = path[::-1]
+                        for cell in cells:
+                            self.window.update()
+                            self.window.after(10,findpath(cell))
+                        return
 
-        start = (np.where(self.board == 2)[0].tolist()[0],
-                 np.where(self.board == 2)[1].tolist()[0])
-        end = (np.where(self.board == 3)[0].tolist()[0],
-               np.where(self.board == 3)[1].tolist()[0])
-        aStar(self.board, start, end)
+                    children = []
+                    # 인접한 xy좌표 전부
+                    for newPosition in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
+
+                        # 노드 위치 업데이트
+                        nodePosition = (
+                            currentNode.position[0] + newPosition[0],  # X
+                            currentNode.position[1] + newPosition[1])  # Y
+
+                        # 미로 maze index 범위 안에 있어야함
+                        within_range_criteria = [
+                            nodePosition[0] > (len(maze) - 1),
+                            nodePosition[0] < 0,
+                            nodePosition[1] > (len(maze[len(maze) - 1]) - 1),
+                            nodePosition[1] < 0,
+                        ]
+
+                        if any(within_range_criteria):  # 하나라도 true면 범위 밖임
+                            continue
+
+                        # 장애물이 있으면 다른 위치 불러오기
+                        if maze[nodePosition[0]][nodePosition[1]] == 1:
+                            continue
+
+                        new_node = Node(currentNode, nodePosition)
+                        children.append(new_node)
+
+                    # 자식들 모두 loop
+                    for child in children:
+
+                        # 자식이 closedList에 있으면 continue
+                        if child in closedList:
+                            continue
+
+                        # f, g, h값 업데이트
+                        child.g = currentNode.g + 1
+                        child.h = ((child.position[0] - endNode.position[0]) **
+                                2) + ((child.position[1] - endNode.position[1]) ** 2)
+                        child.f = child.g + child.h
+
+                        # 자식이 openList에 있으고, g값이 더 크면 continue
+                        if len([openNode for openNode in openList
+                                if child == openNode and child.g > openNode.g]) > 0:
+                            continue
+                        
+                        def drawlist(child):
+                            self.imgcanvas.create_rectangle(
+                            (child.position[1])*length, (child.position[0])*length, (child.position[1]+1)*length, (child.position[0]+1)*length, fill="dodger blue")
+                            self.window.update()
+                        self.window.update()
+                        self.window.after(10,drawlist(child))
+                        openList.append(child)
+                        # time.sleep(0.5)
+
+            start = (np.where(self.board == 2)[0].tolist()[0],
+                    np.where(self.board == 2)[1].tolist()[0])
+            end = (np.where(self.board == 3)[0].tolist()[0],
+                np.where(self.board == 3)[1].tolist()[0])
+            aStar(self.board, start, end)
 
 
     def filesave(self):
@@ -290,7 +296,20 @@ class AStarPathFinding:
         self.canvas.create_image(180, 648, image=self.pnu)
         self.canvas.create_image(1240, 650, image=self.camel)
         imgpath = "Images/"+self.filename+".png"
-        img = Image.open(imgpath)
+        try:
+            img = Image.open(imgpath)
+        except IOError :
+            self.writeMessage("There is no image")
+            return
+        try:
+            coordfile=open("Coordinates/"+self.filename+".txt")
+            self.mapNW=coordfile.readline().split(",")
+            self.mapNE=coordfile.readline().split(",")
+            self.mapSW=coordfile.readline().split(",")
+            self.mapSE=coordfile.readline().split(",")
+        except IOError :
+            self.writeMessage("There is no coordinate file")
+            return
         if img.size[0] > img.size[1]: # 가로로 김 
             img_resize=img.resize((1400,int(img.size[1]*(1400/img.size[0]))))
             if img_resize.size[1]%14 !=0 :
@@ -374,7 +393,10 @@ class AStarPathFinding:
             return "0"+str(num)
         else:
             return "00"+str(num)
-
+    def getcoordinates(self, x, y):
+        eachX=(float(self.mapNE[0])-float(self.mapNW[0]))/horizontalStepCount
+        eachY=(float(self.mapSW[1])-float(self.mapNW[1]))/verticalStepCount
+        self.writeMessage(str(float(self.mapNW[0])+eachX*x)+", "+str(float(self.mapNW[1])+eachY*y))
     def click(self, event):
         if self.boradOn==True :
             grid_position = [event.x, event.y]
@@ -404,7 +426,7 @@ class AStarPathFinding:
                             self.goalcount -= 1
                     else:
                         self.deleteRec(logical_position)
-
+            self.getcoordinates(logical_position[0],logical_position[1])
     def writeMessage(self, string):
         self.cmdwindow.config(state="normal")
         self.cmdwindow.insert(INSERT,string+"\n\n")
